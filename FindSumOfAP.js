eb9dd30ad24bc069aae6e1d079104212cf104e35
codershub-ing <154464457+codershub-ing@users.noreply.github.com>
2024-02-08 17:22:43 +0000

    s

--- FindSumOfAP.js
+++ FindSumOfAP.js
@@ -1,2 +1,22 @@
-var a = parseInt(document.getElementById("first-term").value)
-console.log(a)
+calculateAP.addEventListener("click",function(){
+    let a = parseInt(document.getElementById("first-term").value);
+    let d = parseInt(document.getElementById("common-difference").value);
+    let n = parseInt(document.getElementById("total-terms").value);
+ 
+    let result = (n/2)*((2*a)+((n-1)*d))
+document.getElementById("resultSample1").innerText=result
+let ap = [];
+let currentTerm = a;
+for (let i = 0; i<n; i++) {
+  
+ 
+  ap.push(currentTerm);
+  currentTerm += d
+ console.log(ap)
+
+
+    
+}
+document.getElementById("AP").innerText="S ={ " +ap+" } "
+    console.log("hi")
+})
